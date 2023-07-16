01f23793c5e428e10387163e1d2448b1defc90d5
sirichai10 <prasoppolsirichai@gmail.com>
2023-07-16 20:15:25 +0700

    update firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 //firebaseConfig in your firebase project
 const firebaseConfig = {
-  apiKey: "......",
-  authDomain: "......",
-  projectId: "........",
-  storageBucket: ".....",
-  messagingSenderId: "......",
-  appId: ".......",
-  measurementId: "......."
+  apiKey: "AIzaSyDGtie2_05_wkJKisLmMpFsEWNe3jiaKZo",
+  authDomain: "bookingroom-e4f9f.firebaseapp.com",
+  projectId: "bookingroom-e4f9f",
+  storageBucket: "bookingroom-e4f9f.appspot.com",
+  messagingSenderId: "492917038193",
+  appId: "1:492917038193:web:33c23d317a690b98b70fd0",
+  measurementId: "G-KPG9E4BJGB"
 };
 
 // Initialize Firebase
